05aa5a9eb7ab21a76df4e0a96effea50d562a7d5
stuctu99 <148343592+stuctu99@users.noreply.github.com>
2024-03-14 15:46:26 +0900

    [level 0] Title: 홀짝 구분하기, Time: 196.23 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String answer = n+" ";
+        
+        answer += n%2==0 ? "is even" : "is odd";
+        
+        System.out.println(answer);
+        
+        
+    }
+}
